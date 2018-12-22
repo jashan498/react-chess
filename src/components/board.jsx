0cb084fd0e3c4e5dfea3c2fdda9ff9b19f1ea986
jashan498 <jashansingh.4398@gmail.com>
2018-12-22 19:38:33 +0530

    Added board and square components

---
+++ src/components/board.jsx
@@ -0,0 +1,27 @@
+import React, { Component } from "react";
+import Square from "./square.jsx";
+
+class Board extends Component {
+  renderSquare = (i, squareShade) => {
+    return <Square shade={squareShade} />;
+  };
+  buildBoard = () => {
+    const board = [];
+    for (let i = 0; i < 8; i++) {
+      const sqaureRows = [];
+      for (let j = 0; j < 8; j++) {
+        const squareShade =
+          ((i ^ j) & 1) === 0 ? "square-white" : "square-black";
+        // both i and j has to be even fro square to be white, hence XOR.
+        sqaureRows.push(this.renderSquare(i * 8 + j, squareShade));
+      }
+      board.push(<div className="board-row">{sqaureRows}</div>);
+    }
+    return board;
+  };
+  render() {
+    return <div>{this.buildBoard()}</div>;
+  }
+}
+
+export default Board;
