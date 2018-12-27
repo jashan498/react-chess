8825095b620f1ba663518059b40d830eaace98a7
jashan498 <jashansingh.4398@gmail.com>
2018-12-27 21:58:53 +0530

    Updates

---
+++ precache-manifest.5e128b4c8b8797fe3b5ecd467a948ea9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "6d59ff1669b08a0f02d9",
+    "url": "/static/js/main.6d59ff16.chunk.js"
+  },
+  {
+    "revision": "2cd45fa888a91d926c9e",
+    "url": "/static/js/1.2cd45fa8.chunk.js"
+  },
+  {
+    "revision": "6d59ff1669b08a0f02d9",
+    "url": "/static/css/main.902c9c35.chunk.css"
+  },
+  {
+    "revision": "2cd45fa888a91d926c9e",
+    "url": "/static/css/1.13972918.chunk.css"
+  },
+  {
+    "revision": "c17b97e513d3d0d8644ab8820a3cbbae",
+    "url": "/index.html"
+  }
+];
